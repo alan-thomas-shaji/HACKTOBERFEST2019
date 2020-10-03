df4ffb5a92b38705f568a750af07671cb4b83851
innatepanda <61987098+innatepanda@users.noreply.github.com>
2020-10-03 21:35:25 +0530

    Update HelloWorld.java
    hacktober

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld    {
     public static void main(String a[]){
         System.out.print("Hello World!!");
+        
+        public static void main(String name){
+        System.out.print("Hello World by "+name+" !!");
     }
 }
